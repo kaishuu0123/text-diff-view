4d0769f0f79ba2f9e0905cc2b0241652793c89b8
Koki Oyatsu <kaishuu0123@gmail.com>
2024-11-08 00:44:09 +0900

    Update setAppUserModelId

--- src/main/index.ts
+++ src/main/index.ts
@@ -52,7 +52,7 @@ function createWindow(): void {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('me.saino.text-diff-view')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
